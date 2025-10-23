20dc9d3cbdcfa11839e8acbdf6a1f2f83815eca1
kolli1 <37273607+kolli1@users.noreply.github.com>
2025-10-23 17:25:25 +0530

    Update qservice-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,33 @@
-const CACHE_NAME = 'mochi-inventory-v1';
-const urlsToCache = ['./', './index.html', './manifest.json'];
+const CACHE_NAME = "mochi-inventory-v1";
+const ASSETS_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
 
-self.addEventListener('install', event=>{
-  event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(urlsToCache)));
-});
-
-self.addEventListener('fetch', event=>{
-  event.respondWith(caches.match(event.request).then(response=>response||fetch(event.request)));
-});
-
-self.addEventListener('activate', event=>{
+// Install service worker and cache assets
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k))))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS_TO_CACHE))
+  );
+});
+
+// Serve files from cache, fallback to network
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
+
+// Update cache on version change
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))
+    )
   );
 });
